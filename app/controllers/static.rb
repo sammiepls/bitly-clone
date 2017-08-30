75f39347ca7bb2082dd5303528e3f9c4e326031d
sammiepls <samantha.lhe@gmail.com>
2017-08-30 14:13:21 +0800

    Add table and displays previous URLs

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -1,4 +1,5 @@
 get '/' do
+  @url = Url.all
   erb :"static/index"
 end
 
@@ -6,15 +7,17 @@ post '/urls' do
   url = Url.new(ori_url:params[:ori_url])
   if url.save
     p "#{url.short_url}"
-    @url = Url.find(id)
+    redirect "/"
   else
-    render "static/index"
+    @url = Url.all
+    @errors = url.errors.messages[:url].join.capitalize + " : " + url.errors.messages[:ori_url].join.capitalize
+    erb :"static/index"
   end
 end
 
 # i.e. /q6bda
-get '/:short_url' do
-  # redirect to appropriate "long" URL
-  url = Url.find_by(params[:short_url])
-  redirect url.ori_url
-end
+# get '/:short_url' do
+#   # redirect to appropriate "long" URL
+#   url = Url.find_by(params[:short_url])
+#   redirect url.ori_url
+# end
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,10 +1,22 @@
 require 'securerandom'
+require 'uri'
 
 class Url < ActiveRecord::Base
   before_create :shorten
   validates :ori_url, presence: true
+  validate :url_format
+
 
 	def shorten
     self.short_url = SecureRandom.hex(5)
   end
+
+  def url_format
+    begin
+      errors.add(:url, "Invalid url") unless URI(self.ori_url).is_a?(URI::HTTP)
+    rescue URI::InvalidURIError
+      errors.add(:url, "Invalid url")
+    end
+  end
+
 end
