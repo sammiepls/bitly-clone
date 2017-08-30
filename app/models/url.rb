8c02c881de38dd13194619763a030a75ea15d101
sammiepls <samantha.lhe@gmail.com>
2017-08-30 10:42:13 +0800

    Add styling

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,5 +1,10 @@
+require 'securerandom'
+
 class Url < ActiveRecord::Base
+  before_create :shorten
+  validates :ori_url, presence: true
+
 	def shorten
-    self.SecureRandom.hex(10)
+    self.short_url = SecureRandom.hex(5)
   end
 end
