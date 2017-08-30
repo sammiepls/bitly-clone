a94b3c30a6e7373c8bace291e51691497fea048b
sammiepls <samantha.lhe@gmail.com>
2017-08-30 15:44:53 +0800

    Add click count functionality and dropped old invalid database

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -8,16 +8,20 @@ post '/urls' do
   if url.save
     p "#{url.short_url}"
     redirect "/"
+
   else
     @url = Url.all
-    @errors = url.errors.messages[:url].join.capitalize + " : " + url.errors.messages[:ori_url].join.capitalize
+    @errors = url.errors.messages[:url].join.capitalize + " " + url.errors.messages[:ori_url].join.capitalize
     erb :"static/index"
   end
 end
 
-# i.e. /q6bda
-# get '/:short_url' do
-#   # redirect to appropriate "long" URL
-#   url = Url.find_by(params[:short_url])
-#   redirect url.ori_url
-# end
+# i.e. /q6bdaams[
+get '/:short_url' do
+  # redirect to appropriate "long" URL
+  url = Url.find_by(short_url:params["short_url"])
+
+  url.click_count += 1
+  url.save
+  redirect "#{url.ori_url}"
+end
